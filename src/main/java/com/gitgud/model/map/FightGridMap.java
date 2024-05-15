0dddc657e63a57ed095beba2c299eb64b5e33088
Finn Lindig <Lindig@swms.de>
2024-05-15 14:22:58 +0200

    minor

--- src/main/java/com/gitgud/model/map/FightGridMap.java
+++ src/main/java/com/gitgud/model/map/FightGridMap.java
@@ -1,30 +1,24 @@
 package com.gitgud.model.map;
 
+import com.gitgud.model.gameObjects.gridMovable.FightAgent;
+
+import java.util.HashMap;
+
+
 //
-public class FightGridMap extends GridMap
+public class FightGridMap extends GridMap<FightAgent>
 {
     private static final int EDGE_LENGTH = 12;
     
     
-    public FightGridMap(Tile[][] tiles)
+    public FightGridMap(HashMap<Tile, FightAgent> graph, int width, int height)
     {
-        super(tiles, graph, width, height);
-        
-        checkValidity(tiles);
-    }
-    
-    
-    private static void validateDimensions(Tile[][] tiles)
-    {
-        if (tiles.length != EDGE_LENGTH || tiles[0].length != EDGE_LENGTH)
+        super(graph, width, height);
+        if (width != EDGE_LENGTH || height != EDGE_LENGTH)
         {
             throw new IllegalArgumentException("FightGridMap Argument has wrong dimensions");
         }
     }
     
     
-    private void checkValidity(Tile[][] tiles)
-    {
-        validateDimensions(tiles);
-    }
 }
