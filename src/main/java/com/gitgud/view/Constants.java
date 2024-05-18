0bf568ee7648174eff870ae649760a8a320bb530
delfinaxx <delfina.sara.weyhe@gmail.com>
2024-05-19 00:10:48 +0200

    Added new classes bc of OOP, created new scenes

---
+++ src/main/java/com/gitgud/view/Constants.java
@@ -0,0 +1,21 @@
+package com.gitgud.view;
+
+/**
+ * @author Delfina
+ * @version 1.0
+ * @Owner: Delfina
+ * @since 15.05.2024
+ * Eine Klasse für Konstanten
+ */
+public class Constants {
+    private static final double BUTTON_BREITE = 400;
+    private static final double BUTTON_HOEHE = 90;
+
+    public static final double getButtonBreite(){
+        return BUTTON_BREITE;
+    }
+    public static final double getButtonHoehe() {
+        return BUTTON_HOEHE;
+    }
+}
+
--- src/main/java/com/gitgud/view/SStory.java
+++ src/main/java/com/gitgud/view/SStory.java
@@ -1,25 +1,50 @@
 package com.gitgud.view;
 
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * @author Delfina
+ * @version 1.0
+ * @Owner: Delfina
+ * @since 15.05.2024
+ */
 public class SStory {
 
 
 
     public static Scene createStoryScene(Stage stage)
     {
-        Group root = new Group();
-        Scene StoryScene = new Scene(root);
+        VBox root = GuiUtils.createVBox("C:/Users/delfi/Desktop/2 Semester 2024/GUI_prog/Papier.jpg");
+        Scene StoryScene = new Scene(root, 800, 600);
         stage.setScene(StoryScene);
 
-        // Hier kannst du alles machen
+        // VBox für den Text
+        VBox textBox = new VBox();
+        textBox.setAlignment(Pos.CENTER);
+        textBox.setPadding(new Insets(50));
+        textBox.setSpacing(20);
 
-        Button button = new Button("Continuo");
+        Text text = new Text("blablabla");
+        text.setFont(Font.font("Courier", 24)); // Retro Schriftart und Größe festlegen
+        text.setFill(Color.BLACK); // Textfarbe festlegen
+
+        Button button = new Button("Continue");
+        GuiUtils.setButtonTrait(button);
+
         button.setOnAction(e -> stage.setScene(STutorial.createTutorialScene(stage)));
-        root.getChildren().addAll(button);
+
+        textBox.getChildren().addAll(text, button);
+        root.getChildren().addAll(textBox);
         return StoryScene;
+
+
     }
 }
