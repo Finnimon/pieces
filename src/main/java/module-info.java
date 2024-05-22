3ce2d55f7a01fa70956a43fe147eebaf96620a99
Finn Lindig <Lindig@swms.de>
2024-05-22 13:56:30 +0200

    migrated AssetParser AssetLocator etc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.gitgud.pieces {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+    requires com.google.gson;
+    
+    
     opens com.gitgud.pieces to javafx.fxml;
     exports com.gitgud.pieces;
 }
