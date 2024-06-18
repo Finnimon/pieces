25dd3f5f3ecd136f1a9c1ea26c8cd5ca4fa72ea5
Finn Lindig <Lindig@swms.de>
2024-06-18 18:35:31 +0200

    Added testable gridMap

--- src/main/java/com/gitgud/pieces/testing/TestAssets.java
+++ src/main/java/com/gitgud/pieces/testing/TestAssets.java
@@ -1,6 +1,10 @@
 package com.gitgud.pieces.testing;
 
+import com.gitgud.engine.model.gameObject.GridMappable;
+import com.gitgud.engine.model.map.GridMap;
+
+
 public interface TestAssets
 {
-
+    GridMap<GridMappable> testMap=TestStuff.getTestMap(10,12);
 }
