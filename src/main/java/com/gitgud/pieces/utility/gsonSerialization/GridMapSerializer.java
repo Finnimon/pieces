5502a04c5a7af1e2459ce7c693567637da17f1b3
fimax24 <fimax24@gmail.com>
2024-06-18 19:10:49 +0200

    FightAgentCollectableSerializer hinzugefügt

---
+++ src/main/java/com/gitgud/pieces/utility/gsonSerialization/GridMapSerializer.java
@@ -0,0 +1,32 @@
+package com.gitgud.pieces.utility.gsonSerialization;
+
+import com.gitgud.engine.model.gameObject.GridMappable;
+import com.gitgud.engine.model.map.GridMap;
+import com.google.gson.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GridMapSerializer implements JsonSerializer<GridMap<GridMappable>>, JsonDeserializer<GridMap<GridMappable>>
+{
+    @Override
+    public GridMap<GridMappable> deserialize(JsonElement src, Type type, JsonDeserializationContext context) throws JsonParseException
+    {
+        JsonArray rows = new JsonArray();
+
+        for (String key : src.getAsJsonObject().keySet())
+        {
+            rows = src.getAsJsonObject().get(key).getAsJsonArray();
+        }
+
+        boolean[][] map = context.deserialize(rows, boolean.class);
+    }
+
+    @Override
+    public JsonElement serialize(GridMap src, Type typeOfSrc, JsonSerializationContext context) {
+        return null;
+    }
+}
