ddc7a4340286e9e6872d240b79a4a74e20f2ef4d
jrohe <julius.rohe2004@gmail.com>
2024-06-24 15:40:10 +0200

    Rendering CommentsRendering Comments

---
+++ src/main/java/com/gitgud/pieces/view/SAreana.java
@@ -0,0 +1,26 @@
+package com.gitgud.pieces.view;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+    public class SAreana
+    {
+
+        public static Scene createArenaScene(Stage stage)
+        {
+
+            Group root = new Group();
+            Scene ArenaScene =new Scene(root);
+
+
+            root.getChildren().addAll();
+
+            return ArenaScene;
+        }
+}
