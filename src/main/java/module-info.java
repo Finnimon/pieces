40f4962d021690db3eb7f7346977d3ebb7686506
fimax24 <fimax24@gmail.com>
2024-06-25 21:46:50 +0200

    Neue Modulverweise für Gson angelegt.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,14 +4,13 @@ module com.gitgud.pieces {
     requires com.google.gson;
     requires jdk.unsupported;
     requires com.opencsv;
-    requires jeromq;
-
-
+    
+    
     opens com.gitgud.pieces to javafx.fxml;
     opens com.gitgud.pieces.utility.modification.fightAgent to com.google.gson;
-    opens com.gitgud.engine.model.map to com.google.gson;
-    opens com.gitgud.graph to com.google.gson;
-    opens com.gitgud.pieces.model.activeGame to com.google.gson;
+    opens com.gitgud.pieces.model.gameobjects.agents to com.google.gson;
+    opens com.gitgud.engine.model.gameobjects.agent to com.google.gson;
+    opens com.gitgud.engine.model.gameobjects to com.google.gson;
     exports com.gitgud.pieces;
     exports com.gitgud.pieces.model.gameobjects to com.google.gson;
     exports com.gitgud.engine.model.gameobjects to com.google.gson;
