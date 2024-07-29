5d99494fccb9a2a45b0fc0f925e4436e97242272
Finn Lindig <Lindig@swms.de>
2024-07-29 17:45:48 +0200

    Vertex comments

--- src/main/java/com/gitgud/graph/Vertex.java
+++ src/main/java/com/gitgud/graph/Vertex.java
@@ -1,10 +1,26 @@
 package com.gitgud.graph;
 
+
+/**
+ * A Vertex on a {@link Graph}
+ *
+ * @author Finn Lindig
+ * @Owner: Finn Lindig
+ * @since 10.05.2024
+ * @version 1.1
+ */
 public interface Vertex extends Comparable<Vertex>
 {
+    
+    /**
+     * Getter for the index of this Vertex on its containing {@link Graph}
+     * @return the index of this Vertex on its containing {@link Graph}
+     */
     int getIndex();
     
-    
+    /**
+     * setter for the index of this Vertex on its containing {@link Graph}
+     */
     void setIndex(int index);
     
     
