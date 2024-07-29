6da641c0a31c79fcff47736b98fb21b1cca0dc40
Finn Lindig <Lindig@swms.de>
2024-07-29 18:05:13 +0200

    Commented

--- src/main/java/com/gitgud/graph/WeightedGraph.java
+++ src/main/java/com/gitgud/graph/WeightedGraph.java
@@ -4,14 +4,36 @@ import java.util.HashSet;
 import java.util.TreeMap;
 
 
+/**
+ * A {@link Graph} with {@link WeightedEdge}s as {@link Edge}s
+ *
+ * @param <Vertex>
+ * @param <Element>
+ * @author Finn Lindig
+ * @version 1.0
+ * @Owner: Finn Lindig
+ * @see WeightedEdge
+ * @since 10.05.2024
+ */
 public class WeightedGraph<Vertex extends com.gitgud.graph.Vertex, Element> extends Graph<Vertex, Element, WeightedEdge<Vertex>>
 {
+    /**
+     * default Constructor
+     *
+     * @param vertices The Vertice map for this Graph
+     * @param edges    The Edges for this Graph
+     * @Precondition: All Vertices in {@param edges} are in {@param vertices} also and the other way around and their indexing is intact.
+     * @Postcondition: The Weighted Graph will function properly
+     */
     public WeightedGraph(TreeMap<Vertex, Element> vertices, TreeMap<Vertex, HashSet<WeightedEdge<Vertex>>> edges)
     {
         super(vertices, edges);
     }
     
     
+    /**
+     * Default Constructor that creates and Empty Weighted Graph
+     */
     public WeightedGraph()
     {
     }
